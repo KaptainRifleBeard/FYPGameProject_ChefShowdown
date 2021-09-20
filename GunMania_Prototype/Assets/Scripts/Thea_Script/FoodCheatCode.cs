f2f185f1110fa2ae8f83d39b63fb7dadd90568c0
Thea Hoo <0126943@kdu-online.com>
2021-09-20 16:57:18 +0800

    Food Cheat Code added

--- GunMania_Prototype/Assets/Scripts/Thea_Script/FoodCheatCode.cs
+++ GunMania_Prototype/Assets/Scripts/Thea_Script/FoodCheatCode.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class FoodCheatCode : MonoBehaviour
 {
+    [Header("Food Prefabs")]
+    public List<GameObject> prefabs;
+
+    [Header("Player")]
+    public Transform playerPOS;
+    Vector3 offset = new Vector3(0, 2, 10);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,15 @@ public class FoodCheatCode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("1"))
+        {
+            Instantiate(prefabs[0], playerPOS.position + offset, Quaternion.identity);
+            Debug.Log("apple");
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            Instantiate(prefabs[1], playerPOS.position + offset, Quaternion.identity);
+            Debug.Log("cherry");
+        }
     }
 }
