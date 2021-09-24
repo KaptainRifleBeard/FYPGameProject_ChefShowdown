5c72ffaa65080534f0241249b14de25e79d4a38d
Thea Hoo <0126943@kdu-online.com>
2021-09-24 13:30:13 +0800

    food combination basic

--- GunMania_Prototype/Assets/Scripts/Adam_Script/GameObjects/FoodPickup.cs
+++ GunMania_Prototype/Assets/Scripts/Adam_Script/GameObjects/FoodPickup.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class FoodPickup : MonoBehaviour
 {
-    private Inventory inventory;
+    //private Inventory inventory;
 
-    private void Start()
-    {
-        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
-    }
+    //private void Start()
+    //{
+    //    inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    //}
 
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player") || other.CompareTag("Player2"))
-        {
-            for (int i = 0; i < inventory.slots.Length; i++)
-            {
-                if(inventory.isFull[i] == false)
-                {
-                    inventory.isFull[i] = true;
-                    Debug.Log("FoodStored");
-                    Destroy(gameObject);
-                    break;
-                }
-            }
-        }
-    }
+    //public void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Player") || other.CompareTag("Player2"))
+    //    {
+    //        for (int i = 0; i < inventory.slots.Length; i++)
+    //        {
+    //            if(inventory.isFull[i] == false)
+    //            {
+    //                inventory.isFull[i] = true;
+    //                Debug.Log("FoodStored");
+    //                Destroy(gameObject);
+    //                break;
+    //            }
+    //        }
+    //    }
+    //}
     //scraped
 }
