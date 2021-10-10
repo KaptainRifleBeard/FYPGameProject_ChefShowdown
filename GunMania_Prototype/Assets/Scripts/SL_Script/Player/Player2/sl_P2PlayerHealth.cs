00578b287f29115d25a24a7c3e9815974d44b525
SelinaLim <escales1018@gmail.com>
2021-10-10 16:04:33 +0800

    Update sl_P2PlayerHealth.cs

--- GunMania_Prototype/Assets/Scripts/SL_Script/Player/Player2/sl_P2PlayerHealth.cs
+++ GunMania_Prototype/Assets/Scripts/SL_Script/Player/Player2/sl_P2PlayerHealth.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.AI;
 using Photon.Pun;
-
+using UnityEngine.SceneManagement;
 
 public class sl_P2PlayerHealth : MonoBehaviour
 {
