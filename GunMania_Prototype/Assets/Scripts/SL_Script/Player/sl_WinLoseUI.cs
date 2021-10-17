7978b92555895ecb9b56c5303f3dd1850737216a
SelinaLim <escales1018@gmail.com>
2021-10-17 11:04:32 +0800

    winlose screen button

--- GunMania_Prototype/Assets/Scripts/SL_Script/Player/sl_WinLoseUI.cs
+++ GunMania_Prototype/Assets/Scripts/SL_Script/Player/sl_WinLoseUI.cs
@@ -6,10 +6,11 @@ using Photon.Pun;
 using Photon.Realtime;
 using Photon.Pun.UtilityScripts;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class sl_WinLoseUI : MonoBehaviourPunCallbacks
 {
-    public Text text;
+    public TMP_Text text;
 
     public static readonly byte RestartEventCode = 1;
 
@@ -54,16 +55,16 @@ public class sl_WinLoseUI : MonoBehaviourPunCallbacks
 
     }
 
-    IEnumerator RestartGame()
-    {
-        sl_PlayerHealth.currentHealth = 8;
-        sl_P2PlayerHealth.p2currentHealth = 8;
-        yield return new WaitForSeconds(1.0f);
+    //IEnumerator RestartGame()
+    //{
+    //    sl_PlayerHealth.currentHealth = 8;
+    //    sl_P2PlayerHealth.p2currentHealth = 8;
+    //    yield return new WaitForSeconds(1.0f);
 
-        RaiseEventOptions eventOpt = new RaiseEventOptions { Receivers = ReceiverGroup.All };
-        ExitGames.Client.Photon.SendOptions sendOptions = new ExitGames.Client.Photon.SendOptions { Reliability = true };
-        PhotonNetwork.RaiseEvent(RestartEventCode, null, eventOpt, sendOptions);
-    }
+    //    RaiseEventOptions eventOpt = new RaiseEventOptions { Receivers = ReceiverGroup.All };
+    //    ExitGames.Client.Photon.SendOptions sendOptions = new ExitGames.Client.Photon.SendOptions { Reliability = true };
+    //    PhotonNetwork.RaiseEvent(RestartEventCode, null, eventOpt, sendOptions);
+    //}
 
     IEnumerator WaitStartGame()
     {
@@ -78,6 +79,7 @@ public class sl_WinLoseUI : MonoBehaviourPunCallbacks
         {
             if (PhotonNetwork.IsMasterClient)
             {
+                text.text = "Click to leave room";
                 loseScreen.SetActive(true);
             }
             else
@@ -90,6 +92,7 @@ public class sl_WinLoseUI : MonoBehaviourPunCallbacks
         {
             if (PhotonNetwork.IsMasterClient)
             {
+                text.text = "Click to leave room";
                 winScreen.SetActive(true);
             }
             else
@@ -109,10 +112,10 @@ public class sl_WinLoseUI : MonoBehaviourPunCallbacks
         SceneManager.LoadScene(0);
     }
 
-    IEnumerator DisplayMessage(string message)
-    {
-        text.text = message;
-        yield return new WaitForSeconds(2f);
-        text.text = " ";
-    }
+    //IEnumerator DisplayMessage(string message)
+    //{
+    //    text.text = message;
+    //    yield return new WaitForSeconds(2f);
+    //    text.text = " ";
+    //}
 }
