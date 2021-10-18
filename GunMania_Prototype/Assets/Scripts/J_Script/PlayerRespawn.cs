bcfe06ef8f5ffef1ea475e940557e73e77c8694c
SelinaLim <escales1018@gmail.com>
2021-10-18 14:43:14 +0800

    Revert "Merge branch 'Jared_Tech' into Tech"
    This reverts commit 70b7153533ab3f233b66b142b3b1a7f74a198255, reversing
    changes made to e1cc31f800fc5701b0622d8b04c6450048285c91.

--- GunMania_Prototype/Assets/Scripts/J_Script/PlayerRespawn.cs
+++ GunMania_Prototype/Assets/Scripts/J_Script/PlayerRespawn.cs
@@ -7,15 +7,8 @@ public class PlayerRespawn : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
 
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        //StartCoroutine(OnTriggerEnter());
-        Invoke("OnTriggerEnter", 2);
-    }
-
-    public void OnTriggerEnter(Collider other)
-    {
-        //yield return new WaitForSeconds(2f);
         player.transform.position = respawnPoint.transform.position;
     }
 }
