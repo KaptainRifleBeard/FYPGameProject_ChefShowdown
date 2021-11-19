cea2f1f14dcd27bb84e471d6a6b7b23dd71a65ee
Thea Hoo <0126943@kdu-online.com>
2021-11-19 21:12:18 +0800

    dish effect script added

--- GunMania_Prototype/Assets/Scripts/Thea_Script/Food/DishEffect.cs
+++ GunMania_Prototype/Assets/Scripts/Thea_Script/Food/DishEffect.cs
@@ -2,47 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DishEffect : ScriptableObject
+public class DishEffect : MonoBehaviour
 {
-    public string itemName;
-    public Sprite sprite;
-    public float damage;
-    public float recover;
+    public sl_PlayerHealth player1Health;
 
-}
 
-[CreateAssetMenu(menuName = "Items/RoyalHotpot")]
-public class RoyalHotpot : DishEffect
-{
-    public float RoyalHotpotDMG()
+    private void OnTriggerEnter(Collider other)
     {
-        return damage;
+
+        if (other.gameObject.tag == "Sinseollo")
+        {
+            SinseolloEffect();
+        }
+        else if (other.gameObject.tag == "Tojangjochi")
+        {
+            Tojangjochi();
+        }
+        else if (other.gameObject.tag == "Hassun")
+        {
+            Hassun();
+        }
+        else if (other.gameObject.tag == "Mukozuke")
+        {
+            Mukozuke();
+        }
+        else if (other.gameObject.tag == "BirdNestSoup")
+        {
+            BirdNestSoup();
+        }
+        else if (other.gameObject.tag == "BuddhaJumpsOvertheWall")
+        {
+            BuddhaJumpsOvertheWall();
+        }
+        else if (other.gameObject.tag == "FoxtailMillet")
+        {
+            FoxtailMillet();
+        }
+        else if (other.gameObject.tag == "RawStinkyTofu")
+        {
+            RawStinkyTofu();
+        }
     }
-}
 
-[CreateAssetMenu(menuName = "Items/Hassun")]
-public class Hassun : DishEffect
-{
-    public float HassunHeal()
+        public void SinseolloEffect()
     {
-        return recover;
+        sl_PlayerHealth.currentHealth -= 3;
     }
-}
 
-[CreateAssetMenu(menuName = "Items/BuddhaJumpsOverTheWall")]
-public class BuddhaJumpsOverTheWall : DishEffect
-{
-    public float BuddhaJumpsOverTheWallDMG()
+    public void Tojangjochi()
     {
-        return damage;
+        //stun player code
     }
-}
 
-[CreateAssetMenu(menuName = "Items/FoxtailMilletCake")]
-public class FoxtailMilletCake : DishEffect
-{
-    public float FoxtailMilletCakeDMG()
+    public void Hassun()
     {
-        return damage;
+        sl_PlayerHealth.currentHealth += 3;
+    }
+
+    public void Mukozuke()
+    {
+        sl_PlayerHealth.currentHealth -= 2;
+        //movement
+        //stun
+    }
+
+    public void BirdNestSoup()
+    {
+        //aoe damage???
+    }
+
+    public void BuddhaJumpsOvertheWall()
+    {
+        sl_PlayerHealth.currentHealth -= 2;
+        //silence
+    }
+
+    public void FoxtailMillet()
+    {
+        sl_PlayerHealth.currentHealth -= 2;
+        //knockback
+    }
+
+    public void RawStinkyTofu()
+    {
+        //drop food :')
     }
 }
