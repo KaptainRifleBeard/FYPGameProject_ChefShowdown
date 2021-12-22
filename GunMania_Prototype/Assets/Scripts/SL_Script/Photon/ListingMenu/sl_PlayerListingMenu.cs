4564bc2d5c23d964742746517a3bc2f91ceb1cdc
SelinaLim <66066738+Selina-Lim@users.noreply.github.com>
2021-12-22 08:42:41 +0800

    edit visual studio settings

--- GunMania_Prototype/Assets/Scripts/SL_Script/Photon/ListingMenu/sl_PlayerListingMenu.cs
+++ GunMania_Prototype/Assets/Scripts/SL_Script/Photon/ListingMenu/sl_PlayerListingMenu.cs
@@ -94,10 +94,9 @@ public class sl_PlayerListingMenu : MonoBehaviourPunCallbacks
     {
         int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
 
-        if (PhotonNetwork.IsMasterClient && playerCount == 2)  // room owner
+        if (PhotonNetwork.IsMasterClient/* && playerCount == 2*/)  // room owner
         {
             PhotonNetwork.LoadLevel("sl_TestScene");
-
         }
         PhotonNetwork.CurrentRoom.IsOpen = false;
 
