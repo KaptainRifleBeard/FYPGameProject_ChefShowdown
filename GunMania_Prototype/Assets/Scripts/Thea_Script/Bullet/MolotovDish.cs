48d45aa893d77de0197ad4e7f38e5b78061f1629
Thea Hoo <0126943@kdu-online.com>
2022-01-14 03:43:09 +0800

    molotov bullet function added (no damage yet)

---
+++ GunMania_Prototype/Assets/Scripts/Thea_Script/Bullet/MolotovDish.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MolotovDish : MonoBehaviour
+{
+    public bool isPlayer;
+    private bool isHit;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(isPlayer)
+        {
+            if (collision.gameObject.tag == "Environment" || collision.gameObject.tag == "Player2")
+            {
+                GetComponent<MeshRenderer>().enabled = false;
+                GetComponent<SphereCollider>().enabled = true;
+                isHit = true;
+            }
+        }
+        else if(!isPlayer)
+        {
+            if (collision.gameObject.tag == "Environment" || collision.gameObject.tag == "Player")
+            {
+                GetComponent<MeshRenderer>().enabled = false;
+                GetComponent<SphereCollider>().enabled = true;
+                isHit = true;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isHit = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isHit)
+        {
+            StartCoroutine(DestroySelf());
+        }
+    }
+
+    public IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(3);
+
+        Destroy(gameObject);
+    }
+}
