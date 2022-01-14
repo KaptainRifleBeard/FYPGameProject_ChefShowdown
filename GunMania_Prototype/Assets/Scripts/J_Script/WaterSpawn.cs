3d33cab59d184244e0899d2c87c7b09a2cbbe4f0
SelinaLim <escales1018@gmail.com>
2022-01-14 15:08:13 +0800

    add description

--- GunMania_Prototype/Assets/Scripts/J_Script/WaterSpawn.cs
+++ GunMania_Prototype/Assets/Scripts/J_Script/WaterSpawn.cs
@@ -15,6 +15,15 @@ public class WaterSpawn : MonoBehaviour
         InvokeRepeating("SpawnWater", spawnTime, spawnDelay);
 
         //after 5secs stop, wait 10secs spawn again. so its 15secs
+
+        /*
+        10, 20, 30sec delay, 3sec shoot 
+
+         1st = 10 + 3 = 13sec
+        2nd = 13 + 13 = 26sec
+        3rd = 26 + 13 = 39sec
+         
+         */
     }
 
     public void SpawnWater()
