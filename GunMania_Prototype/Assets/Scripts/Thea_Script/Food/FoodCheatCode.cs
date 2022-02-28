a62b05e9045272205623299aff525a2e39a5d9a6
Thea Hoo <0126943@kdu-online.com>
2022-02-28 13:10:37 +0800

    update
    can't really test what happened :')

--- GunMania_Prototype/Assets/Scripts/Thea_Script/Food/FoodCheatCode.cs
+++ GunMania_Prototype/Assets/Scripts/Thea_Script/Food/FoodCheatCode.cs
@@ -12,6 +12,9 @@ public class FoodCheatCode : MonoBehaviour
     public Transform playerPOS;
     Vector3 offset = new Vector3(0, 2, 10);
 
+
+    int count;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,45 +24,57 @@ public class FoodCheatCode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("1"))
+        if (count < 1 && PhotonNetwork.IsMasterClient)
         {
-            PhotonNetwork.Instantiate(prefabs[0].name, playerPOS.position + offset, Quaternion.identity);
-            
-        }
-        else if (Input.GetKeyDown("2"))
-        {
-            PhotonNetwork.Instantiate(prefabs[1].name, playerPOS.position + offset, Quaternion.identity);
-            
-        }
-        else if (Input.GetKeyDown("3"))
-        {
-            PhotonNetwork.Instantiate(prefabs[2].name, playerPOS.position + offset, Quaternion.identity);
+            if (Input.GetKeyDown("1"))
+            {
+                PhotonNetwork.Instantiate(prefabs[0].name, playerPOS.position, Quaternion.identity);
+                count++;
 
-        }
-        else if (Input.GetKeyDown("4"))
-        {
-            PhotonNetwork.Instantiate(prefabs[3].name, playerPOS.position + offset, Quaternion.identity);
+            }
+            else if (Input.GetKeyDown("2"))
+            {
+                PhotonNetwork.Instantiate(prefabs[1].name, playerPOS.position, Quaternion.identity);
+                count++;
 
-        }
-        else if (Input.GetKeyDown("5"))
-        {
-            PhotonNetwork.Instantiate(prefabs[4].name, playerPOS.position + offset, Quaternion.identity);
+            }
+            else if (Input.GetKeyDown("3"))
+            {
+                PhotonNetwork.Instantiate(prefabs[2].name, playerPOS.position, Quaternion.identity);
+                count++;
 
-        }
-        else if (Input.GetKeyDown("6"))
-        {
-            PhotonNetwork.Instantiate(prefabs[5].name, playerPOS.position + offset, Quaternion.identity);
+            }
+            else if (Input.GetKeyDown("4"))
+            {
+                PhotonNetwork.Instantiate(prefabs[3].name, playerPOS.position, Quaternion.identity);
+                count++;
 
-        }
-        else if (Input.GetKeyDown("7"))
-        {
-            PhotonNetwork.Instantiate(prefabs[6].name, playerPOS.position + offset, Quaternion.identity);
+            }
+            else if (Input.GetKeyDown("5"))
+            {
+                PhotonNetwork.Instantiate(prefabs[4].name, playerPOS.position, Quaternion.identity);
+                count++;
 
-        }
-        else if (Input.GetKeyDown("8"))
-        {
-            PhotonNetwork.Instantiate(prefabs[7].name, playerPOS.position + offset, Quaternion.identity);
+            }
+            else if (Input.GetKeyDown("6"))
+            {
+                PhotonNetwork.Instantiate(prefabs[5].name, playerPOS.position, Quaternion.identity);
+                count++;
 
+            }
+            else if (Input.GetKeyDown("7"))
+            {
+                PhotonNetwork.Instantiate(prefabs[6].name, playerPOS.position, Quaternion.identity);
+                count++;
+
+            }
+            else if (Input.GetKeyDown("8"))
+            {
+                PhotonNetwork.Instantiate(prefabs[7].name, playerPOS.position, Quaternion.identity);
+                count++;
+
+            }
         }
+        
     }
 }
