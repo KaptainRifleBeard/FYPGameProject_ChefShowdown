04c40449af8012cd105e8dad761d2699d9cdf326
SelinaLim <escales1018@gmail.com>
2022-03-02 18:24:59 +0800

    Update FoodCheatCode.cs

--- GunMania_Prototype/Assets/Scripts/Thea_Script/Food/FoodCheatCode.cs
+++ GunMania_Prototype/Assets/Scripts/Thea_Script/Food/FoodCheatCode.cs
@@ -12,8 +12,12 @@ public class FoodCheatCode : MonoBehaviour
     public Transform playerPOS;
     Vector3 offset = new Vector3(0, 2, 10);
 
-
+    public static bool startCheatCde;
     int count;
+    private void Start()
+    {
+        startCheatCde = false;
+    }
 
     // Update is called once per frame
     void Update()
@@ -23,52 +27,65 @@ public class FoodCheatCode : MonoBehaviour
             if (Input.GetKeyDown("1"))
             {
                 PhotonNetwork.Instantiate(prefabs[0].name, playerPOS.position, Quaternion.identity);
-                count++;
+                //count++;
+                //StartCoroutine(reset());
 
             }
             else if (Input.GetKeyDown("2"))
             {
                 PhotonNetwork.Instantiate(prefabs[1].name, playerPOS.position, Quaternion.identity);
-                count++;
+                //count++;
+                //StartCoroutine(reset());
 
             }
             else if (Input.GetKeyDown("3"))
             {
                 PhotonNetwork.Instantiate(prefabs[2].name, playerPOS.position, Quaternion.identity);
-                count++;
+                //count++;
+                //StartCoroutine(reset());
 
             }
             else if (Input.GetKeyDown("4"))
             {
                 PhotonNetwork.Instantiate(prefabs[3].name, playerPOS.position, Quaternion.identity);
-                count++;
+                //count++;
+                //StartCoroutine(reset());
 
             }
             else if (Input.GetKeyDown("5"))
             {
                 PhotonNetwork.Instantiate(prefabs[4].name, playerPOS.position, Quaternion.identity);
-                count++;
+                //count++;
+                //StartCoroutine(reset());
 
             }
             else if (Input.GetKeyDown("6"))
             {
                 PhotonNetwork.Instantiate(prefabs[5].name, playerPOS.position, Quaternion.identity);
-                count++;
+                //count++;
+                //StartCoroutine(reset());
 
             }
             else if (Input.GetKeyDown("7"))
             {
                 PhotonNetwork.Instantiate(prefabs[6].name, playerPOS.position, Quaternion.identity);
-                count++;
+                //count++;
+                //StartCoroutine(reset());
 
             }
             else if (Input.GetKeyDown("8"))
             {
                 PhotonNetwork.Instantiate(prefabs[7].name, playerPOS.position, Quaternion.identity);
-                count++;
-
+                //count++;
+                //StartCoroutine(reset());
             }
         }
         
+    }
+
+    IEnumerator reset()
+    {
+        yield return new WaitForSeconds(0.5f);
+        count = 0;
     }
 }
