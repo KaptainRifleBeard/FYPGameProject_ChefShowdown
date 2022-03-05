110d6958c61bcd9282c9b909e0c42578ff03da43
SelinaLim <escales1018@gmail.com>
2022-03-06 02:15:47 +0800

    update in world sfx

--- GunMania_Prototype/Assets/Scripts/J_Script/SoundTrigger.cs
+++ GunMania_Prototype/Assets/Scripts/J_Script/SoundTrigger.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class SoundTrigger : MonoBehaviour
 {
-    //[SerializeField] private AudioSource stallsAudio;
-    //[SerializeField] private AudioSource stallsAudio2;
+    //public AudioClip[] stallsClips;
+    public AudioSource[] stallsAudio;
 
-    public AudioClip[] stallsClips;
-    private AudioSource stallsAudio;
-
-    public float soundDelay;
+    //public float soundDelay;
 
     //private AudioClip RandomClip
     //{
@@ -24,26 +21,98 @@ public class SoundTrigger : MonoBehaviour
     //{
     //    if(!stallClips)
     //}
+    float num;
 
     void Start()
     {
-        stallsAudio = GetComponent<AudioSource>();
+        //stallsAudio = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player" || other.gameObject.tag == "Player2")
         {
-            //stallsAudio.Play();
-            stallsAudio.clip = stallsClips[Random.Range(0, stallsClips.Length)];
-            stallsAudio.PlayDelayed(soundDelay);
+            num = Random.Range(0, stallsAudio.Length);
+
+            if(num == 0)
+            {
+                stallsAudio[0].Play();
+                stallsAudio[0].volume = 0.5f;
+
+            }
+            if (num == 1)
+            {
+                stallsAudio[1].Play();
+                stallsAudio[1].volume = 0.5f;
+
+            }
+            if (num == 2)
+            {
+                stallsAudio[2].Play();
+                stallsAudio[2].volume = 0.5f;
+
+            }
+
+            //stallsAudio.clip = stallsClips[Random.Range(0, stallsClips.Length)];
+            //stallsAudio.volume += 0.2f;
+
+            //stallsAudio.PlayDelayed(soundDelay);
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        stallsAudio.Stop();
+        StartCoroutine(DecreaseVolumn());
+        //stallsAudio.Stop();
         //stallsAudio.PlayDelayed(soundDelay);
+    }
+
+    IEnumerator DecreaseVolumn()
+    {
+        if (num == 0)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                yield return new WaitForSeconds(0.1f);
+                stallsAudio[0].volume -= 0.05f;
+
+            }
+
+            if (stallsAudio[0].volume <= 0.1)
+            {
+                stallsAudio[0].Stop();
+            }
+        }
+
+        if (num == 1)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                yield return new WaitForSeconds(0.1f);
+                stallsAudio[1].volume -= 0.05f;
+
+            }
+            if (stallsAudio[1].volume <= 0.1)
+            {
+                stallsAudio[1].Stop();
+            }
+        }
+
+        if (num == 2)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                yield return new WaitForSeconds(0.1f);
+                stallsAudio[2].volume -= 0.05f;
+
+            }
+
+            if (stallsAudio[2].volume <= 0.1)
+            {
+                stallsAudio[2].Stop();
+            }
+        }
+
     }
 
 
