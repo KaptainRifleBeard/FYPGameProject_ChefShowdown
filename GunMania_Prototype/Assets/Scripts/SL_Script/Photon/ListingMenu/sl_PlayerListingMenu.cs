a92f16651a73fcd512a2097f6032c68c01adad3b
SelinaLim <escales1018@gmail.com>
2022-03-21 12:30:01 +0800

    update

--- GunMania_Prototype/Assets/Scripts/SL_Script/Photon/ListingMenu/sl_PlayerListingMenu.cs
+++ GunMania_Prototype/Assets/Scripts/SL_Script/Photon/ListingMenu/sl_PlayerListingMenu.cs
@@ -122,8 +122,6 @@ public class sl_PlayerListingMenu : MonoBehaviourPunCallbacks
         if (PhotonNetwork.IsMasterClient && playerCount == 2)  // room owner
         {
             PhotonNetwork.LoadLevel("sl_TestScene");
-            //PhotonNetwork.LoadLevel("TestScene");
-
             startGame = true;
 
         }
